4bf25b51b9693d4679de580e18553decf167b6b8
unknown <sabrincasemiro@gmail.com>
2021-09-19 14:16:25 -0300

    Menu com Requisição API JSON e JS

--- src/script/menu/index.js
+++ src/script/menu/index.js
@@ -4,10 +4,32 @@ const renderMenuDesktop = (menu) => {
     let structureMenu = ''
 
     menu.map((department) => {
-        structureMenu += `
-        <a href=${department.url}>${department.name}</a>`
+        if(department.children === undefined){
+            structureMenu += `
+                <div class='header__menu--wrapper'>
+                    <a href=${department.url} class='menu-item'>${department.name}</a>
+                </div>`
+        } else{
+            structureMenu += `
+                <div class='header__menu--wrapper'>
+                    <a href=${department.url} class='menu-item'>${department.name}</a>
+                        <div class='header__menu--items'>
+                            ${department.children.map((category) => {
+                                if(category.children === undefined){
+                                    return `<a href=${category.url}>${category.name}`
+                                } else{
+                                    return `<a href=${category.url}>${category.name}
+                                    <div class='header__menu--sub-item'>
+                                    ${category.children.map((subcategory) => {
+                                        return `<a href=${subcategory.url}>${subcategory.name}`
+                                    })}`
+                                }
+                            })}
+                        </div>
+                </div>`
+        }
+       
     })
-
     $containerMenu.innerHTML = structureMenu
 }
 
